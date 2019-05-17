ddea4448a3604d6814458a74f4bb3f77bdecc651
Sean Knowles <sean@skdigital.me>
2019-05-17 18:16:55 +0200

    partial initial ui completion

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import {withRouter, NavLink} from "react-router-dom";
 
 function Header() {
-  return <div>Header</div>;
+  return <div className="header">Header
+    <div className="flex">
+      <img src="/logo.png" alt="Hooks News Logo" className="logo"/>
+        <NavLink className="header-title" to="/">Hooks News</NavLink>
+        <NavLink className="header-title" to="/">Hooks News</NavLink>
+    </div>
+  </div>
 }
 
 export default Header;
